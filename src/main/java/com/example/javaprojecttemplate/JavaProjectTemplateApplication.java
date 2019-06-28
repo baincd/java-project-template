2baf3a6a0917bb955740e56765a1f8c9372e81cf
Chris Bain <baincd@users.noreply.github.com>
2019-06-27 20:55:23 -0400

    Add health check following application startup

--- src/main/java/com/example/javaprojecttemplate/JavaProjectTemplateApplication.java
+++ src/main/java/com/example/javaprojecttemplate/JavaProjectTemplateApplication.java
@@ -1,13 +1,23 @@
 package com.example.javaprojecttemplate;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class JavaProjectTemplateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaProjectTemplateApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(JavaProjectTemplateApplication.class, args);
+		
+		Health health = ctx.getBean(HealthEndpoint.class).health();
+		
+		log.info("Application Context health status is {}",health.getStatus());
 	}
 
 }
