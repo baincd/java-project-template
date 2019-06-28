494a043ab55c19f3959e1f6cb49464bea95bb8e4
Chris Bain <baincd@users.noreply.github.com>
2019-06-27 20:08:37 -0400

    Spring Initializr v2.1.6

---
+++ src/main/java/com/example/javaprojecttemplate/JavaProjectTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaprojecttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectTemplateApplication.class, args);
+	}
+
+}
